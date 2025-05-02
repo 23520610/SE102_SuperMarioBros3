769602d3131928f04e83b369c4d42aed13ce8c6b
23520610 <167666218+23520610@users.noreply.github.com>
2025-05-02 23:57:56 +0700

    Gan xong paragoomba

---
+++ SE102_SuperMarioBros3/ParaGoomba.cpp
@@ -0,0 +1,210 @@
+﻿#include "ParaGoomba.h"
+#include "Goomba.h"
+#include "debug.h"
+
+CParaGoomba::CParaGoomba(float x, float y, float spawnX) : CGoomba(x, y, spawnX)
+{
+    isActive = false;
+    hasWings = true;
+    isOnGround = false;
+    ay = PARA_GOOMBA_GRAVITY;
+    SetState(GOOMBA_STATE_WALKING);
+    hopCount = 0;
+    last_action_time = GetTickCount64();
+}
+
+void CParaGoomba::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+    CGoomba::Update(dt, coObjects);
+
+    if (hasWings && state != GOOMBA_STATE_DIE)
+    {
+        ULONGLONG now = GetTickCount64();
+        if (isOnGround)
+        {
+            if (hopCount == 5)
+            {
+                if (now - last_action_time >= PARA_GOOMBA_WAIT_TIME)
+                {
+                    hopCount = 0;
+                    last_action_time = now;
+                    SetState(GOOMBA_STATE_WALKING);
+                }
+                else
+                {
+                    if (state != GOOMBA_STATE_WALKING)
+                        SetState(GOOMBA_STATE_WALKING);
+                    return;
+                }
+            }
+            if (hopCount == 0 && now - last_action_time >= PARA_GOOMBA_HOP_INTERVAL)
+            {
+                vy = -PARA_GOOMBA_SMALL_HOP_SPEED;
+                hopCount = 1;
+                SetState(GOMBA_STATE_HOPPING);
+                last_action_time = now;
+                DebugOut(L"[INFO] Start small hop, hopCount = 1\n");
+                return;
+            }
+
+            // Nhảy nhỏ tiếp theo
+            if (hopCount >= 1 && hopCount < NUM_SMALL_HOPS && now - last_action_time >= PARA_GOOMBA_HOP_INTERVAL)
+            {
+                vy = -PARA_GOOMBA_SMALL_HOP_SPEED;
+                hopCount++;
+                SetState(GOMBA_STATE_HOPPING);
+                last_action_time = now;
+                DebugOut(L"[INFO] Continue small hop, hopCount = %d\n", hopCount);
+                return;
+            }
+
+            // Nhảy lớn
+            if (hopCount == NUM_SMALL_HOPS && now - last_action_time >= PARA_GOOMBA_HOP_INTERVAL)
+            {
+                vy = -PARA_GOOMBA_BIG_JUMP_SPEED;
+                hopCount = 4;
+                SetState(GOMBA_STATE_FLYING);
+                last_action_time = now;
+                DebugOut(L"[INFO] Big jump, hopCount = 4\n");
+                return;
+            }
+        }
+
+        // Chạm đất sau nhảy lớn
+        if (hopCount == 4 && isOnGround)
+        {
+            hopCount = 5;
+            last_action_time = now;
+            SetState(GOOMBA_STATE_WALKING);
+            DebugOut(L"[INFO] Landed after big jump, hopCount = 5\n");
+            return;
+        }
+    }
+
+    if (!hasWings && state != GOOMBA_STATE_DIE && vx == 0)
+    {
+        vx = -GOOMBA_WALKING_SPEED;
+    }
+}
+
+void CParaGoomba::Render()
+{
+    int aniId = -1;
+
+    if (state == GOOMBA_STATE_DIE)
+    {
+        aniId = ID_ANI_PARA_GOOMBA_DIE;
+    }
+    else if (hasWings)
+    {
+        if (state == GOMBA_STATE_FLYING)
+        {
+            aniId = ID_ANI_PARA_GOOMBA_JUMPING;
+        }
+        else if (state == GOMBA_STATE_HOPPING)
+        {
+            aniId = ID_ANI_PARA_GOOMBA_HOPING;
+        }
+        else
+        {
+            aniId = ID_ANI_PARA_GOOMBA_WALKING_WINGED;
+        }
+    }
+    else
+    {
+        aniId = ID_ANI_PARA_GOOMBA_WALK;
+    }
+
+    if (aniId != -1) {
+        CAnimations::GetInstance()->Get(aniId)->Render(x, y);
+    }
+	//RenderBoundingBox();
+}
+
+void CParaGoomba::SetState(int state)
+{
+    CGameObject::SetState(state);
+
+    switch (state)
+    {
+    case GOOMBA_STATE_DIE:
+        die_start = GetTickCount64();
+        y += (GOOMBA_BBOX_HEIGHT - GOOMBA_BBOX_HEIGHT_DIE) / 2;
+        vx = 0;
+        vy = 0;
+        ay = 0;
+        hopCount = 0;
+        break;
+
+    case GOOMBA_STATE_WALKING:
+        vx = -GOOMBA_WALKING_SPEED;
+        ay = PARA_GOOMBA_GRAVITY;
+        break;
+
+    case GOMBA_STATE_HOPPING:
+    case GOMBA_STATE_FLYING:
+        break;
+    }
+}
+
+void CParaGoomba::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+    if (state == GOOMBA_STATE_DIE)
+    {
+        left = x - PARA_GOOMBA_BBOX_WIDTH / 2;
+        top = y - PARA_GOOMBA_BBOX_HEIGHT_DIE / 2;
+        right = left + PARA_GOOMBA_BBOX_WIDTH;
+        bottom = top + PARA_GOOMBA_BBOX_HEIGHT_DIE;
+    }
+    else if (hasWings)
+    {
+		if (state == GOMBA_STATE_FLYING)
+		{
+			left = x - PARA_GOOMBAWING_BBOX_WIDTH / 2;
+			top = y - PARA_GOOMBA_FLYING_BBOX_HEIGHT / 2;
+			right = left + PARA_GOOMBAWING_BBOX_WIDTH;
+			bottom = top + PARA_GOOMBA_FLYING_BBOX_HEIGHT;
+		}
+		else
+		{
+			left = x - PARA_GOOMBAWING_BBOX_WIDTH / 2;
+			top = y - PARA_GOOMBAWING_BBOX_HEIGHT / 2;
+			right = left + PARA_GOOMBAWING_BBOX_WIDTH;
+			bottom = top + PARA_GOOMBAWING_BBOX_HEIGHT;
+		}
+        
+    }
+    else
+    {
+        left = x - PARA_GOOMBA_BBOX_WIDTH / 2;
+        top = y - PARA_GOOMBA_BBOX_HEIGHT / 2;
+        right = left + PARA_GOOMBA_BBOX_WIDTH;
+        bottom = top + PARA_GOOMBA_BBOX_HEIGHT;
+    }
+}
+
+void CParaGoomba::OnCollisionWith(LPCOLLISIONEVENT e)
+{
+    CGoomba::OnCollisionWith(e);
+    //va chạm xử lý bên mario
+    if (!e->obj->IsBlocking()) return;
+
+    if (e->ny != 0)
+    {
+        if (!isOnGround)
+        {
+            isOnGround = true;
+            DebugOut(L"[INFO] ParaGoomba is on ground, hopCount: %d\n", hopCount);
+        }
+    }
+}
+
+void CParaGoomba::OnNoCollision(DWORD dt)
+{
+    CGoomba::OnNoCollision(dt);
+    if (isOnGround)
+    {
+        isOnGround = false;
+    }
+}
+
---
+++ SE102_SuperMarioBros3/ParaGoomba.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#include "Goomba.h"
+#include "FireBall.h"
+#include "PlayScene.h"
+
+#define PARA_GOOMBA_SMALL_HOP_SPEED  0.2f
+#define PARA_GOOMBA_BIG_JUMP_SPEED   0.5f 
+#define NUM_SMALL_HOPS 3
+#define PARA_GOOMBA_HOP_INTERVAL 300 
+#define PARA_GOOMBA_WAIT_TIME 400
+#define PARA_GOOMBA_GRAVITY GOOMBA_GRAVITY
+
+#define PARA_GOOMBAWING_BBOX_WIDTH 20
+#define PARA_GOOMBAWING_BBOX_HEIGHT 18
+#define PARA_GOOMBA_FLYING_BBOX_HEIGHT 23
+#define PARA_GOOMBA_BBOX_WIDTH GOOMBA_BBOX_WIDTH
+#define PARA_GOOMBA_BBOX_HEIGHT GOOMBA_BBOX_HEIGHT
+#define PARA_GOOMBA_BBOX_HEIGHT_DIE GOOMBA_BBOX_HEIGHT_DIE
+
+#define GOMBA_STATE_HOPPING 300
+#define GOMBA_STATE_FLYING 400
+
+#define ID_ANI_PARA_GOOMBA_WALK 170000
+#define ID_ANI_PARA_GOOMBA_WALKING_WINGED 170001
+#define ID_ANI_PARA_GOOMBA_HOPING 170002
+#define ID_ANI_PARA_GOOMBA_JUMPING 170003
+#define ID_ANI_PARA_GOOMBA_DIE 170004
+
+class CParaGoomba : public CGoomba
+{
+protected:
+    bool hasWings;
+    bool isOnGround = false;
+    int hopCount = 0; // 0: chờ, 1-3: nhảy nhỏ, 4: nhảy lớn, 5: chờ 400ms
+    ULONGLONG last_action_time = 0;
+
+    virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom) override;
+    virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) override;
+    virtual void Render() override;
+    virtual void SetState(int state) override;
+
+    virtual void OnCollisionWith(LPCOLLISIONEVENT e) override;
+    virtual void OnNoCollision(DWORD dt) override;
+
+public:
+    CParaGoomba(float x, float y, float spawnX);
+    virtual int IsBlocking() { return 0; }
+    virtual int IsCollidable() { return state != GOOMBA_STATE_DIE; }
+	bool getHasWings() { return hasWings; }
+	void lostWings() 
+    {
+        hasWings = false;
+        hopCount = 0;
+        last_action_time = GetTickCount64();
+        SetState(GOOMBA_STATE_WALKING);
+        vy = 0; 
+        return;
+    }
+};
